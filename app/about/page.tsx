d080424231907bd7ba2e276107fbeb6e7eabcdc2
Satyam1Vishwakarma <97113954+Satyam1Vishwakarma@users.noreply.github.com>
2024-10-10 17:46:14 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,26 +1,28 @@
+import { GithubIcon } from "@/components/icons";
 import { title } from "@/components/primitives";
+import { Link } from "@nextui-org/link";
 import { Spacer } from "@nextui-org/spacer";
 import React from "react";
 
 const Aboutme = () => {
   return (
     <center className="text-center py-20">
-      <div>
-        <u>
-          <h1 className={title({ color: "blue", size: "lg" })}>About Me</h1>
-        </u>
-      </div>
+      <u>
+        <h1 className={title({ color: "blue", size: "lg" })}>About Me</h1>
+      </u>
       <Spacer y={5}></Spacer>
-      <div>
-        <h1 className={title({ color: "pink", size: "md" })}>
-          Satyam Vishwakarma
-        </h1>
-      </div>
-      <div>
-        <h1 className={title({ color: "cyan", size: "sm" })}>
-          Computer Science<br></br>Student<br></br>@GPA
-        </h1>
-      </div>
+      <h1 className={title({ color: "pink", size: "md" })}>
+        Satyam Vishwakarma
+        <br />
+      </h1>
+      <h1 className={title({ color: "cyan", size: "sm" })}>
+        Computer Science<br />Student<br />@GPA
+      </h1>
+      <center>
+        <Link href="https://github.com/Satyam1Vishwakarma">
+          <GithubIcon height={60} width={80} size={35} />
+        </Link>
+      </center>
     </center>
   );
 };
